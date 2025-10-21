f28ffbc139791ee4c49a74dfc17815b25cbce4de
Ajith Solomon Sekar Jayakumar <ajithsolomonsekarj@gmail.com>
2025-10-21 20:38:29 +0200

    Squashed commit of the following:
    commit eb4b1418ab4bc6b1820aebedf52ddc555613716e
    Author: Ajith Solomon Sekar Jayakumar <ajithsolomonsekarj@gmail.com>
    Date:   Fri Oct 17 08:21:38 2025 +0200
    
        Add main application class and basic configuration

---
+++ src/main/java/com/ajith/reposcorer/RepoScorerApplication.java
@@ -0,0 +1,12 @@
+package com.ajith.reposcorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoScorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepoScorerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ajith/reposcorer/RepoScorerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ajith.reposcorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoScorerApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
